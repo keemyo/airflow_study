0b69c5f5dfca428a708983d2fd09bc30f610e1c4
keemyo <creactive0510@gmail.com>
2024-09-27 19:06:26 +0900

    0927 study

---
+++ airflow/dags/01_unscheduled.py
@@ -0,0 +1,41 @@
+# (예약 안된) 이벤트 DAG의 버전 초기화(dag/01_unscheduled.py)
+import datetime as dt from pathlib import Path
+import pandas as pd
+from airflow import DAG
+from airflow.operator.bash import BashOperator 
+from airflow.operator.python import PythonOperator
+
+dag = DAG(
+    dag_id = "01_unscheduled",    
+    start_date = dt.datetime(year=2019, month=1, day=1),
+    schedule_interval=None
+)
+
+fetch_events = BashOperator(
+    task_id = "fetch_events",
+    bash_command = (
+        "mkdir -p /data &&"
+        "curl -o /data/events.json"
+        "https://localhost:5000/events" # API에서 이벤트를 가져온 후 저장
+    ),
+    dag=dag,
+)
+
+def _calculate_stats(input_path, output_path):
+    """이벤트 통계 계산기"""
+    events = pd.read_json(input_path)
+    stats = events.groupby(['date', 'user']).size().reset_index() # 이벤트 데이터를 로드하고 필요한 통계를 계산 
+    path(output_path).parent.mkdir(exist_ok=True) # 추ㄹ력 디렉토리가 있는지 확인하고 결과를 csv로 저장
+    stats.to_csv(output_path, index=False) 
+
+calculate_stats = PythonOperator(
+    task_id = "calculate_stats",
+    python_callable = _calculate_stats,
+    op_kwargs = {
+        "input_path": "/data/events.json",
+        "output_path": "/data_stats.csv"
+    },
+    dag = dag
+)
+
+fetch_events >> calculate_stats
--- download_rocket_launches.py
+++ download_rocket_launches.py
@@ -4,55 +4,59 @@ import airflow
 import requests 
 import requests.exceptions as requests_exceptions
 from airflow import DAG
-from airflow.operators.bash import BashOperators
+from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-dag = DAG( #객체의 인스턴스 생성(구체화) 모든 워크플로우의 시작점
-    dag_id = 'download_rocket_launches', # Dag 이름
-    start_date = airflow.utils.dates.days_ago(14), # Dag 처음 실행 시작 날짜
-    schedule_interval = None, # Dag 실행간격
+dag = DAG(
+    dag_id='download_rocket_launches',  # DAG 이름
+    start_date=airflow.utils.dates.days_ago(14),  # 시작 날짜
+    schedule_interval= "@daily",  # 실행 간격
 )
 
-download_launches = BashOperators( # BashOperator를 이용해 curl로 URL 결괏값 다운로드
-    task_id = 'download_launches', # 테스크이름 
-    bash_command = "curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'", 
-    dag=dag,    
+# BashOperator를 이용해 URL로부터 데이터를 다운로드
+download_launches = BashOperator(
+    task_id='download_launches',  # 테스크 이름
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",
+    dag=dag
 )
 
-def _get_pictures(): # 파이썬 함수는 결괏값을 파싱하고 모든 로켓 사진을 다운로드
-    # 경로가 존재하는지 확인(디렉토리확인)
-    pathlib.Path("/tmp/images").mkdir(parents=True, exists_ok=True)
+# 파이썬 함수를 사용하여 이미지 다운로드
+def _get_pictures():
+    # 경로가 존재하는지 확인
+    images_path = pathlib.Path("/tmp/images")
+    if not images_path.exists():
+        images_path.mkdir(parents=True)
 
-    # launches.json 파일에 있는 모든 그림 다운로드
+    # launches.json 파일을 열고 이미지 다운로드
     with open("/tmp/launches.json") as f:
         launches = json.load(f)
         image_urls = [launch["image"] for launch in launches["results"]]
-        for imge_url in image_urls:
+        for image_url in image_urls:
             try:
                 response = requests.get(image_url)
                 image_filename = image_url.split("/")[-1]
-                target_file = f"/tmp/images/{image_filename""image_filename}"
+                target_file = f"/tmp/images/{image_filename}"
                 with open(target_file, 'wb') as f:
                     f.write(response.content)
                 print(f"Downloaded {image_url} to {target_file}")
             except requests_exceptions.MissingSchema:
-                print(f'{image_irl} appears to be an invalid URL.')
+                print(f'{image_url} appears to be an invalid URL.')
             except requests_exceptions.ConnectionError:
                 print(f'Could not connect to {image_url}.')
 
-get_pictures = PythonOperator( #DAG에서 PythonOprator를 사용하여 파이썬 함수 호출 
-    task_id = "get_pictures",
-    python_callable = _get_pictures,
-    dag = dag
+# PythonOperator를 사용하여 파이썬 함수 실행
+get_pictures = PythonOperator(
+    task_id="get_pictures",
+    python_callable=_get_pictures,
+    dag=dag
 )
 
-notify = BashOperators(
-    task_id = 'notify',
-    bash_command = 'echo "There are now $(ls /tmp/images/ | wc -l) images."',
-    dag = dag
+# 이미지 개수를 알리는 BashOperator
+notify = BashOperator(
+    task_id='notify',
+    bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
+    dag=dag
 )
 
-download_launches >> get_pictures >> notify # 태스크 실행 순서
-
-
-                          
+# DAG의 실행 순서 정의
+download_launches >> get_pictures >> notify
